0a976d59af6022164ac3cf9b3b8c4d522dac2aa2
Karim <Karmo>
2020-03-24 01:21:39 +0100

    add flirOne photos compatibillytie

--- CNN.py
+++ CNN.py
@@ -23,7 +23,7 @@ IMG_Channels = 1
 # Load the data into tf
 images = []
 labels = []
-path_to_image = "FinalDataSet/Images/*.BMT"
+path_to_image = "FinalDataSet/Images/*.*"
 path_to_labels = "FinalDataSet/FinalDataPoints.csv"
 
 
@@ -31,29 +31,70 @@ def getImageName(image_path):
     img_parts = image_path.split(os.path.sep)
     return img_parts[-1]
 
-
 index = 0
 one_image = 0
 one_label = 0
 
-def size(img, labels, scale):
+# show one image with spots
+def showOneImg(image, label):
+    fillTheCircle = -1
+    color = (0, 0, 0)
+    radius = 2
+    coordianates1 = (label[0], label[1])
+    img = cv2.circle(image, coordianates1, radius,
+                     color, fillTheCircle)
+
+    coordianates2 = (label[2], label[3])
+    img1 = cv2.circle(img, coordianates2, radius,
+                      color, fillTheCircle)
+
+    coordianates3 = (label[4], label[5])
+    img2 = cv2.circle(img1, coordianates3, radius,
+                      color, fillTheCircle)
+    print("Image dimensions: {}".format(image.shape))
+    print("Image labels: {}".format(label))
+    cv2.imshow("TestImage", img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def up_size(img, labels, scale):
     # times of original size
     width = int(img.shape[1] * scale)
     height = int(img.shape[0] * scale)
     dim = (width, height)
     # resize image
-    img_resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    img_resized = cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
     labels_resized = []
     for i in labels:
         labels_resized.append(i * scale)
     return img_resized, labels_resized
 
 
+def down_size(img, labels):
+    # times of original size
+    width = int(img.shape[1] / 3.375)
+    height = int(img.shape[0] / 6)
+    dim = (width, height)
+    # resize image
+    img_resized = cv2.resize(img, dim, interpolation=cv2.INTER_NEAREST)
+    labels_resized = []
+    index = 0;
+    for i in labels:
+        if (index % 2 == 0):
+            labels_resized.append(int(i / 3.375))
+        else:
+            labels_resized.append(int(i / 6))
+        index +=1
+    return img_resized, labels_resized
+
+
 def reSizeImgAndLabels(img, labels):
     if (img.shape == (120, 160,IMG_Channels)):
-        return size(img, labels, 2)
+        return up_size(img, labels, 2)
+    elif img.shape == (240, 320, IMG_Channels):
+        return up_size(img, labels, 1)
     else:
-        return size(img, labels, 1)
+        return down_size(img, labels)
 
 def mirrorIMG(image, label):
     fliped_img = cv2.flip(image, 1)
@@ -68,29 +109,6 @@ def mirrorIMG(image, label):
     fliped_labels.append(int(IMG_HEIGHT - label[4]))
     fliped_labels.append(int(IMG_WIDTH - label[5]))
     return fliped_img, fliped_labels
-
-def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 *
-                                                     (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' %
-          (prefix, bar, percent, suffix), end=printEnd)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
 
 print("Image Augementation started")
 totalFiles = len(glob.glob(path_to_image))
@@ -117,29 +135,6 @@ for image_path in glob.glob(path_to_image):
             images.append(imgNormalization)
 
 
-# show one image with spots
-def showOneImg(image, label):
-    fillTheCircle = -1
-    color = (0, 0, 0)
-    radius = 2
-    coordianates1 = (label[0], label[1])
-    img = cv2.circle(image, coordianates1, radius,
-                     color, fillTheCircle)
-
-    coordianates2 = (label[2], label[3])
-    img1 = cv2.circle(img, coordianates2, radius,
-                      color, fillTheCircle)
-
-    coordianates3 = (label[4], label[5])
-    img2 = cv2.circle(img1, coordianates3, radius,
-                      color, fillTheCircle)
-    print("Image dimensions: {}".format(image.shape))
-    print("Image labels: {}".format(label))
-    cv2.imshow("TestImage", img2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 #showOneImg(one_image, one_label)
 npImgArray = np.array(images)
 npLabelArray = np.array(labels)
@@ -164,15 +159,14 @@ model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(units=128, activation="relu"))
 model.add(tf.keras.layers.Dropout(0.25))
 model.add(tf.keras.layers.Dense(units=64, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.50))
+model.add(tf.keras.layers.Dropout(0.5))
 model.add(tf.keras.layers.Dense(units=64, activation="relu"))
-model.add(tf.keras.layers.Dropout(0.25))
 model.add(tf.keras.layers.Dense(units=6))
 
 model.summary()
 
 # Keep only a single checkpoint, the best over test accuracy.
-filepath = "checkpoints/checkpoint-{epoch:04d}.h5"
+filepath = "checkpoints/checkpointBlackAndWhite-{epoch:04d}.h5"
 checkpoint = ModelCheckpoint(filepath,
                             monitor='accuracy',
                             verbose=1,
@@ -180,21 +174,21 @@ checkpoint = ModelCheckpoint(filepath,
                             mode='auto')
 
 
-csv_fileName = "logs/CSV_log.csv"
+csv_fileName = "logs/CSV_log_BlackAndWhite.csv"
 logger = tf.keras.callbacks.CSVLogger(
     csv_fileName, separator=',', append=False
 )
 
 model.compile(loss="mean_absolute_error", optimizer="adam", metrics=["accuracy"])
 
-model.fit(npImgArray, npLabelArray, epochs=10, batch_size=5, validation_split=0.2, callbacks=[checkpoint, logger])
+model.fit(npImgArray, npLabelArray, epochs=800, batch_size=5, validation_split=0.2, callbacks=[checkpoint, logger])
 
-model.save("./savedModels/myModel3.h5")
+model.save("./savedModels/myModelBlackAndWhite.h5")
 
 def test_model(model):
     list_to_validation = "./FinalDataSet/FlirOneTestBilleder/*.jpg"
     for i in range(1, 10):
-        img_resized, labels_resized = size(npImgArray[i], npLabelArray[i], 1)
+        img_resized, labels_resized = reSizeImgAndLabels(npImgArray[i], npLabelArray[i], 1)
         test_img_input = np.reshape(img_resized, (1,240,320,1))
         prediction = model.predict(test_img_input)  # shape = [batch_size, values]
         print(prediction[0])
