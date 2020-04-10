c779f366b3757724dbf93ab14ff10c22afd96034
Karim <Karmo>
2020-04-10 14:01:54 +0200

    now i have a methode to rotate img

--- DataManager.py
+++ DataManager.py
@@ -7,11 +7,10 @@ import csv
 import random
 import math
 
-
 # HyperPerameters
 BATCH_SIZE = 1
-IMG_HEIGHT = int(480/2)
-IMG_WIDTH = int(640/2)
+IMG_HEIGHT = int(480 / 2)
+IMG_WIDTH = int(640 / 2)
 
 # Load the data into tf
 images = []
@@ -19,30 +18,34 @@ labels = []
 path_to_image = "FinalDataSet/Images/*.*"
 path_to_labels = "FinalDataSet/FinalDataPoints.csv"
 
+
 def getImageName(image_path):
     img_parts = image_path.split(os.path.sep)
     return img_parts[-1]
+
 
 index = 0
 one_image = 0
 one_label = 0
 
+
 # show one image with spots
 def showOneImg(image, label=None):
+    inputImg = image.copy()
     fillTheCircle = -1
     radius = 2
     if label is not None:
         coordianates1 = (label[0], label[1])
-        img = cv2.circle(image, coordianates1, radius,
-                         (0,0,255), fillTheCircle)
+        img = cv2.circle(inputImg, coordianates1, radius,
+                         (0, 0, 255), fillTheCircle)
 
         coordianates2 = (label[2], label[3])
         img1 = cv2.circle(img, coordianates2, radius,
-                        (0,255,0), fillTheCircle)
+                          (0, 255, 0), fillTheCircle)
 
         coordianates3 = (label[4], label[5])
         img2 = cv2.circle(img1, coordianates3, radius,
-                          (0,0,0), fillTheCircle)
+                          (0, 0, 0), fillTheCircle)
         print("Image labels: {}".format(label))
     else:
         img2 = image
@@ -50,6 +53,7 @@ def showOneImg(image, label=None):
     cv2.imshow("TestImage", img2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def up_size(img, labels, scale):
     # times of original size
@@ -66,8 +70,8 @@ def up_size(img, labels, scale):
 
 def down_size(img, labels):
     # times of original size
-    scallingWidth = img.shape[1]/240
-    scallingHeigth = img.shape[0]/320
+    scallingWidth = img.shape[1] / 240
+    scallingHeigth = img.shape[0] / 320
 
     width = int(img.shape[1] / scallingWidth)
     height = int(img.shape[0] / scallingHeigth)
@@ -81,11 +85,11 @@ def down_size(img, labels):
             labels_resized.append(int(i / scallingWidth))
         else:
             labels_resized.append(int(i / scallingHeigth))
-        index +=1
+        index += 1
     return img_resized, labels_resized
 
 
-def  reSizeImgAndLabels(img, labels, IMG_Channels):
+def reSizeImgAndLabels(img, labels, IMG_Channels):
     if (img.shape == (120, 160, IMG_Channels)):
         return up_size(img, labels, 2)
     elif img.shape == (240, 320, IMG_Channels):
@@ -93,80 +97,84 @@ def  reSizeImgAndLabels(img, labels, IMG_Channels):
     else:
         return down_size(img, labels)
 
+
 def mirrorImgHorizon(image, label):
-    #img.shape returns  (heigth(Y), width(X), channels)
+    # img.shape returns  (heigth(Y), width(X), channels)
     fliped_img = cv2.flip(image, 1)
     imgWidth = fliped_img.shape[1]
     fliped_labels = []
-    #right eye
+    # right eye
     fliped_labels.append(mirrorXCoordinate(imgWidth, label[0]))
     fliped_labels.append(label[1])
-    #left eye
+    # left eye
     fliped_labels.append(mirrorXCoordinate(imgWidth, label[2]))
     fliped_labels.append(label[3])
-    #nose
+    # nose
     fliped_labels.append(mirrorXCoordinate(imgWidth, label[4]))
     fliped_labels.append(label[5])
     return fliped_img, fliped_labels
 
+
 def mirrorXCoordinate(imgWidth, x):
-    halfimageSide = imgWidth/2
+    halfimageSide = imgWidth / 2
     distancToMidt = halfimageSide - x
     if (distancToMidt == 0):
         return x
-    elif (distancToMidt >0):
+    elif (distancToMidt > 0):
         return int(halfimageSide + distancToMidt)
     else:
         return int(halfimageSide - abs(distancToMidt))
 
-def rotateAndScale(img, scaleFactor = 0.5, degreesCCW = 30):
-    (oldY,oldX) = img.shape[0], img.shape[1] #note: numpy uses (y,x) convention but most OpenCV functions use (x,y)
-    M = cv2.getRotationMatrix2D(center=(oldX/2,oldY/2), angle=degreesCCW, scale=scaleFactor) #rotate about center of image.
 
-    #choose a new image size.
-    newX,newY = oldX*scaleFactor,oldY*scaleFactor
+def rotateAndScale(img, scaleFactor=0.5, degreesCCW=30):
+    (oldY, oldX) = img.shape[0], img.shape[1]  # note: numpy uses (y,x) convention but most OpenCV functions use (x,y)
+    M = cv2.getRotationMatrix2D(center=(oldX / 2, oldY / 2), angle=degreesCCW,
+                                scale=scaleFactor)  # rotate about center of image.
 
-    #include this if you want to prevent corners being cut off
+    # choose a new image size.
+    newX, newY = oldX * scaleFactor, oldY * scaleFactor
+
+    # include this if you want to prevent corners being cut off
     r = np.deg2rad(degreesCCW)
-    newX,newY = (abs(np.sin(r)*newY) + abs(np.cos(r)*newX),abs(np.sin(r)*newX) + abs(np.cos(r)*newY))
+    newX, newY = (abs(np.sin(r) * newY) + abs(np.cos(r) * newX), abs(np.sin(r) * newX) + abs(np.cos(r) * newY))
 
-    #the warpAffine function call, below, basically works like this:
+    # the warpAffine function call, below, basically works like this:
     # 1. apply the M transformation on each pixel of the original image
     # 2. save everything that falls within the upper-left "dsize" portion of the resulting image.
 
-    #So I will find the translation that moves the result to the center of that region.
-    (tx,ty) = ((newX-oldX)/2,(newY-oldY)/2)
-    M[0,2] += tx #third column of matrix holds translation, which takes effect after rotation.
-    M[1,2] += ty
+    # So I will find the translation that moves the result to the center of that region.
+    (tx, ty) = ((newX - oldX) / 2, (newY - oldY) / 2)
+    M[0, 2] += tx  # third column of matrix holds translation, which takes effect after rotation.
+    M[1, 2] += ty
 
-    rotatedImg = cv2.warpAffine(img, M, dsize=(int(newX),int(newY)))
+    rotatedImg = cv2.warpAffine(img, M, dsize=(int(newX), int(newY)))
     return rotatedImg
 
+
 def make_square(img, labels, min_size=320):
-    color = [0, 0, 0] # 'cause black!
+    color = [0, 0, 0]  # 'cause black!
     imgShape = img.shape[:2]
     addBoaderToWidth = min_size - imgShape[1]
     addBoaderToHeigth = min_size - imgShape[0]
 
-    if(addBoaderToWidth <= 0):
-        left, right = 0,0
+    if (addBoaderToWidth <= 0):
+        left, right = 0, 0
     if (addBoaderToHeigth <= 0):
-        top, bottom,= 0,0   
-    if(addBoaderToWidth > 0):
-        left, right = int(addBoaderToWidth/2), int(addBoaderToWidth/2)
+        top, bottom, = 0, 0
+    if (addBoaderToWidth > 0):
+        left, right = int(addBoaderToWidth / 2), int(addBoaderToWidth / 2)
     if (addBoaderToHeigth > 0):
-        top, bottom,= int(addBoaderToHeigth/2), int(addBoaderToHeigth/2)
+        top, bottom, = int(addBoaderToHeigth / 2), int(addBoaderToHeigth / 2)
 
     labels_resized = []
     index = 0
     for i in labels:
         if (index % 2 == 0):
-            labels_resized.append(int(i + (addBoaderToWidth/2)))
+            labels_resized.append(int(i + (addBoaderToWidth / 2)))
         else:
-            labels_resized.append(int(i + (addBoaderToHeigth/2)))
-        index +=1
+            labels_resized.append(int(i + (addBoaderToHeigth / 2)))
+        index += 1
 
-    
     img_with_border = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
     return img_with_border, labels_resized
 
@@ -178,7 +186,7 @@ def getGrayImages():
     IMG_Channels = 1
 
     totalFiles = len(glob.glob(path_to_image))
-    counter =0
+    counter = 0
     for image_path in glob.glob(path_to_image):
         counter += 1
         print("[{}/{}]".format(totalFiles, counter))
@@ -187,12 +195,12 @@ def getGrayImages():
         for row in csv.reader(labels_csv, delimiter=","):
             if (row[0] == image_name):
                 img_raw_labels = [int(round(float(row[1]))), int(round(float(row[2]))), int(round(float(row[3]))),
-                            int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
+                                  int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
                 img = cv2.imread(image_path)
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
                 img = np.expand_dims(img, axis=2)
                 imgResape, labelResape = reSizeImgAndLabels(img, img_raw_labels, IMG_Channels)
-                #one_image, one_label = mirrorIMG(imgResape, labelResape)
+                # one_image, one_label = mirrorIMG(imgResape, labelResape)
                 one_image = imgResape
                 one_label = labelResape
                 imgResape = np.expand_dims(imgResape, axis=2)
@@ -201,6 +209,7 @@ def getGrayImages():
                 images.append(imgNormalization)
     return images, labels
 
+
 def getColorImages():
     print("Image Augementation started")
     print("Resize, toGray and normalization")
@@ -208,7 +217,7 @@ def getColorImages():
     IMG_Channels = 3
 
     totalFiles = len(glob.glob(path_to_image))
-    counter =0
+    counter = 0
     for image_path in glob.glob(path_to_image):
         counter += 1
         print("[{}/{}]".format(totalFiles, counter))
@@ -217,7 +226,7 @@ def getColorImages():
         for row in csv.reader(labels_csv, delimiter=","):
             if (row[0] == image_name):
                 img_raw_labels = [int(round(float(row[1]))), int(round(float(row[2]))), int(round(float(row[3]))),
-                            int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
+                                  int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
                 img = cv2.imread(image_path)
                 imgResape, labelResape = reSizeImgAndLabels(img, img_raw_labels, IMG_Channels)
                 one_image = imgResape
@@ -228,6 +237,7 @@ def getColorImages():
     showOneImg(images[500])
     return images, labels
 
+
 def getColorImagesAsRect():
     print("Image Augementation started")
     print("Resize, toGray and normalization")
@@ -235,7 +245,7 @@ def getColorImagesAsRect():
     IMG_Channels = 3
 
     totalFiles = len(glob.glob(path_to_image))
-    counter =0
+    counter = 0
     for image_path in glob.glob(path_to_image):
         counter += 1
         print("[{}/{}]".format(totalFiles, counter))
@@ -244,50 +254,88 @@ def getColorImagesAsRect():
         for row in csv.reader(labels_csv, delimiter=","):
             if (row[0] == image_name):
                 img_raw_labels = [int(round(float(row[1]))), int(round(float(row[2]))), int(round(float(row[3]))),
-                            int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
+                                  int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
                 img = cv2.imread(image_path)
 
                 imgResape, labelResape = reSizeImgAndLabels(img, img_raw_labels, IMG_Channels)
                 imgNormalization = img / 255.0
 
-                #Add black padding to Img to make the image square
-                imgNormalization, labelResized = make_square(imgNormalization, labelResape,  min_size=320)
+                # Add black padding to Img to make the image square
+                imgNormalization, labelResized = make_square(imgNormalization, labelResape, min_size=320)
 
                 labels.append(labelResized)
                 images.append(imgNormalization)
-    randomIndex = random.randint(0,len(images)-1)
+    randomIndex = random.randint(0, len(images) - 1)
     print('showing image # ', randomIndex)
     showOneImg(images[randomIndex], labels[randomIndex])
-    
+
     return images, labels
 
 
 def distToPoint(x, y, centerpoint):
-    return math.sqrt(math.pow(x-centerpoint[0],2) + math.pow(y-centerpoint[1],2))
+    return math.sqrt(math.pow(x - centerpoint[0], 2) + math.pow(y - centerpoint[1], 2))
 
+
+def rotateROI(x, y, centerPoint, newCenterPoint, degrees):
+    dist = distToPoint(x, y, centerPoint)
+
+    deltaX = x - centerPoint[0]
+    deltaY = y - centerPoint[1]
+    if (y > centerPoint[1]):
+        deltaY = -abs(deltaY)
+    elif (y < centerPoint[1]):
+        deltaY = abs(deltaY)
+
+    if (x > centerPoint[0]):
+        deltaX = abs(deltaX)
+    elif (x < centerPoint[0]):
+        deltaX = -abs(deltaX)
+
+    delta_new_x = deltaX*math.cos(math.radians(degrees))-deltaY*math.sin(math.radians(degrees))
+    delta_new_y = deltaX*math.sin(math.radians(degrees))+deltaY*math.cos(math.radians(degrees))
+
+    if (delta_new_x < 0):
+        newX = newCenterPoint[0] - abs(delta_new_x)
+    elif (delta_new_x > 0):
+        newX = newCenterPoint[0] + delta_new_x
+    else:
+        newX = newCenterPoint[0]
+
+    if (delta_new_y < 0):
+        newY = newCenterPoint[1] + abs(delta_new_y)
+    elif (delta_new_y > 0):
+        newY = newCenterPoint[1] - delta_new_y
+    else:
+        newY = newCenterPoint[1]
+
+    return int(newX), int(newY)
 
 
 def rotateImg(img, labels):
-    rotated_labels  = []
+    rotated_labels = []
+    degrees_To_Rotate = random.randint(0, 360)
 
-    #Getting dimensions
-    imgOrgHeigth = img.shape[0] #Y
-    imgOrgWidth = img.shape[1] #X
-    centerPoint = (int(imgOrgWidth/2), int(imgOrgHeigth/2)) #Format (x, y)
+    # Getting dimensions
+    imgOrgHeigth = img.shape[0]  # Y
+    imgOrgWidth = img.shape[1]  # X
+    centerPoint = (int(imgOrgWidth / 2), int(imgOrgHeigth / 2))  # Format (x, y)
 
-    distance_To_LeftEye = distToPoint(labels[0],labels[1],centerPoint)
-    distance_To_RigthEye = distToPoint(labels[2],labels[3],centerPoint)
-    distance_To_Nose = distToPoint(labels[4],labels[5],centerPoint)
-
-    showOneImg(img, [int(imgOrgWidth/2), int(imgOrgHeigth/2),0,0,0,0])
-
-    rotated_img = rotateAndScale(img, 1, 45) #rotated img is allready a squrer so they sould just be down scaled
+    rotated_img = rotateAndScale(img, 1,
+                                 degrees_To_Rotate)  # rotated img is allready a squrer so they sould just be down scaled
     rotImgHeigth = rotated_img.shape[0]
     rotImgWidth = rotated_img.shape[1]
-    showOneImg(rotated_img, [int(rotImgWidth/2), int(rotImgHeigth/2),0,0,0,0])
-
+    newCenterPoint = (int(rotImgWidth / 2), int(rotImgHeigth / 2))  # Format (x, y)
+    index=0
+    for coordinate in labels:
+        if index % 2 == 0:
+            x, y = rotateROI(labels[index], labels[index+1], centerPoint, newCenterPoint, degrees_To_Rotate)
+            rotated_labels.append(x)
+            rotated_labels.append(y)
+        index += 1
+    showOneImg(rotated_img, rotated_labels)
 
     return rotated_img, rotated_labels
+
 
 def rotateImgsTest():
     print("Image Augementation started")
@@ -308,18 +356,15 @@ def rotateImgsTest():
                                   int(round(float(row[4]))), int(round(float(row[5]))), int(round(float(row[6])))]
                 img = cv2.imread(image_path)
 
-
                 imgResape, labelResape = reSizeImgAndLabels(img, img_raw_labels, IMG_Channels)
 
                 img, labels = rotateImg(imgResape, labelResape)
 
                 print("rotated img shape: ", str(img.shape))
 
-
-
                 imgNormalization = img / 255.0
 
-                labels.append([0,0,0,0,0,0])
+                labels.append([0, 0, 0, 0, 0, 0])
                 images.append(imgNormalization)
     randomIndex = random.randint(0, len(images) - 1)
     print('showing image # ', randomIndex)
@@ -327,12 +372,13 @@ def rotateImgsTest():
 
     return images, labels
 
+
 images, labels = rotateImgsTest()
 
 one_image = images[300]
 one_label = labels[300]
 
-rotated_img = rotateAndScale(one_image, 0.5,30)
+rotated_img = rotateAndScale(one_image, 0.5, 30)
 
 print("normal img")
 showOneImg(one_image, one_label)
