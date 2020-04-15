5369bb19e933496e22be4d76858b825292b83643
karimkuku@gmail.com <karimkuku@gmail.com>
2020-04-15 02:23:02 +0200

    something funky is going one with the traning dataset

--- InceptionV3.py
+++ InceptionV3.py
@@ -26,10 +26,9 @@ global_avg_layer = tf.keras.layers.Flatten()(base_model.output)
 print("The new output layer: ", str(global_avg_layer))
 
 #oure trainable layer
-dense1 = tf.keras.layers.Dense(units=1024, activation="relu")(global_avg_layer)
+dense1 = tf.keras.layers.Dense(units=2048, activation="relu")(global_avg_layer)
 dense2 = tf.keras.layers.Dense(units=1024, activation="relu")(dense1)
-dense3 = tf.keras.layers.Dense(units=1024, activation="relu")(dense2)
-prediction_layer = tf.keras.layers.Dense(units=6)(dense3)
+prediction_layer = tf.keras.layers.Dense(units=6)(dense2)
 
 #combining the network to oure layer
 model = tf.keras.models.Model(inputs=base_model.input, outputs=prediction_layer)
@@ -42,7 +41,14 @@ model.compile(optimizer="adam", loss="mean_absolute_error", metrics=['accuracy']
 
 #Data agumentation
 images, labels = DataManager.GetImgsRotatedAndFliped()
+totalImg = len(images)
 train_Img, train_Lab, validation_Img, validation_Lab, test_Img, test_Lab = DataManager.SplitDataSet(images, labels)
+
+print("TotalImgs: {} TrainSet size: {} validationSet size: {} testSet size: {}".format(totalImg, len(train_Img), len(validation_Img), len(test_Img)))
+print(("showning from training"))
+DataManager.showOneRandomImg(train_Img, train_Lab)
+print("showing from val")
+DataManager.showOneRandomImg(validation_Img, validation_Lab)
 
 modelName = "InceptionV3"
 
@@ -61,9 +67,10 @@ logger = CSVLogger(
 )
 
 #training
-model.fit(train_Img, train_Lab, batch_size=2 , epochs=10, validation_data=(validation_Img, validation_Lab), callbacks=[checkpoint, logger])
+model.fit(train_Img, train_Lab, batch_size=32 , epochs=500, validation_data=(validation_Img, validation_Lab), callbacks=[checkpoint, logger])
 
 model.save("./savedModels/RGB_{}.h5".format(modelName))
 
+print(evaluating)
 loss,acc = model.evaluate(x= test_Img, y=test_Lab)
 print("Model performance:\n loss: {} \n Accuracy: {}".format(loss,acc))
