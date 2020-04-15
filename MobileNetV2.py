fix normalisation

MobileNetV2.py
@@ -10,7 +10,7 @@ from tensorflow_core.python.keras.callbacks import ModelCheckpoint, CSVLogger
 # (width, Heigth)
 IMG_SHAPE = (320, 320, 3)
 
-base_model = tf.keras.applications.inception_v3.InceptionV3(input_shape=IMG_SHAPE, include_top=False, weights="imagenet")
+base_model = tf.keras.applications.mobilenet_v2.MobileNetV2(input_shape=IMG_SHAPE, include_top=False, weights="imagenet")
 
 base_model.summary()
 
@@ -24,6 +24,7 @@ global_avg_layer = tf.keras.layers.Flatten()(base_model.output)
 
 #the new output layer
 print("The new output layer: ", str(global_avg_layer))
+
 
 #oure trainable layer
 dense1 = tf.keras.layers.Dense(units=2048, activation="relu")(global_avg_layer)