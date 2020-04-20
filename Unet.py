ee2929db5d5733837646f367d78678fca8ca0a8f
Karim <Karmo>
2020-04-20 18:02:37 +0200

    updated unet

--- Unet.py
+++ Unet.py
@@ -4,7 +4,7 @@ from tensorflow_core.python.keras.layers import Conv2D, MaxPooling2D, Dropout, U
     Dense, BatchNormalization
 from tensorflow_core.python.keras.optimizers import Adam
 
-
+# inspiration: https://www.kaggle.com/cjansen/u-net-in-keras
 def get_unet(input_Shape):
     inputs = Input(input_Shape)
 
@@ -50,6 +50,59 @@ def get_unet(input_Shape):
     # conv9  = ZeroPadding2D(padding=(ch[0],cw[0]), data_format="channels_last")(conv9)
     # conv10 = Conv2D(1, (1, 1), data_format="channels_last", activation="sigmoid")(conv9)
 
+    flatten = Flatten()(conv9)
+    Dense1 = Dense(512, activation='relu')(flatten)
+    Dense2 = Dense(6)(Dense1)
+
+    model = Model(input=inputs, output=Dense2)
+
+    return model
+
+#Inspiration: https://github.com/zhixuhao/unet/blob/master/model.py
+def unet2(input_size):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    upsam1 = UpSampling2D(size=(2, 2))(drop5)
+    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(upsam1)
+    merge6 = concatenate([drop4, up6], axis=3)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
+
+    upsam2 = UpSampling2D(size=(2, 2))(conv6)
+    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(upsam2)
+    merge7 = concatenate([conv3, up7], axis=3)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
+
+    upsam3 = UpSampling2D(size=(2, 2))(conv7)
+    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(upsam3)
+    merge8 = concatenate([conv2, up8], axis=3)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
+
+    upsam4 = UpSampling2D(size=(2, 2))(conv8)
+    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(upsam4)
+    merge9 = concatenate([conv1, up9], axis=3)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
     flatten = Flatten()(conv9)
     Dense1 = Dense(512, activation='relu')(flatten)
     Dense2 = Dense(6)(Dense1)
